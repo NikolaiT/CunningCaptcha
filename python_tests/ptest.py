fca4427d33031b80267418b7798ea5f6e287cb96
NikolaiT <nikolai@tschacher.ch>
2013-10-27 15:59:35 +0100

    Modified test

--- python_tests/ptest.py
+++ python_tests/ptest.py
@@ -1,19 +1,22 @@
-# Testing speed to draw 500 bezier points with algorithm from.
+# Testing drawing 500 bezier points with algorithm from http://stackoverflow.com/questions/246525/how-can-i-draw-a-bezier-curve-using-pythons-pil/2292690#2292690
 
 import timer
 import random
 from pascal_bezier import *
 
-R = random.randint
-points = [[(R(0,999), R(0,999)), (R(0,999), R(0,999)), (R(0,999), R(0,999))] for i in range(500)]
+NUM_CURVES = 500
+NUM_STEPS = 1000
 
-ts = [t/100.0 for t in range(100)]
+R = random.randint
+points = [[(R(0,999), R(0,999)), (R(0,999), R(0,999)), (R(0,999), R(0,999))] for i in range(NUM_CURVES)]
+
+ts = [t/100.0 for t in range(NUM_STEPS)]
 
 im = Image.new('RGBA', (1000, 1000), (155, 255, 0, 0))
 draw = ImageDraw.Draw(im)
 draw.rectangle([(0,0),(1000,1000)], fill=(0,0,0)) # Black as the night
 
-with timer.Timer('testing') as t:
+with timer.Timer('Testing plotting %u bezier points with Camerons implementation') as t:
 	for p in points:
 		bezier = make_bezier(p)
 		points = bezier(ts)
